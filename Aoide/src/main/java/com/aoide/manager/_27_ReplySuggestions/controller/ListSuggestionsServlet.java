40b9df6b92e1fdc231f1f94afba10da9b9106b08
Billy <Billy@EEIT87.com>
2016-08-31 16:28:53 +0800

    新增點東西

--- Aoide/src/main/java/com/aoide/manager/_27_ReplySuggestions/controller/ListSuggestionsServlet.java
+++ Aoide/src/main/java/com/aoide/manager/_27_ReplySuggestions/controller/ListSuggestionsServlet.java
@@ -24,7 +24,7 @@ public class ListSuggestionsServlet extends HttpServlet {
 		// System.out.println("In ReplySuggestionsServlet doGet()");
 		// get the suggestion list from service
 		List<SuggestionVO> unreplySuggestions = new SuggestionService().getUnreplySuggestions();
-		// put suggestions list into session object
+		// put suggestions list into session objeb  ct
 		for(SuggestionVO aSuggestion : unreplySuggestions){
 			System.out.println(aSuggestion.getSuggestionContentFile());
 		}
