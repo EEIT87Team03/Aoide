2a136f113c36b30c39992e32bb89529d41cc6ac7
oldperson <asd159753117@gmail.tw>
2016-09-03 17:05:27 +0800

    add _99_TestUpload 上傳檔案 > 存檔 > DB存路徑 View: webapp\_99_TestUpload.view\checkUploadResult.jsp 檢視上傳結果頁面 webapp\_99_TestUpload.view\TestUpload.jsp 上傳頁面
    Controller:
    member\_99_TestUpload\controller\TestUploadServlet.java
    
    Model:
    member\_99_TestUpload\model\TestUploadDAO.java
    member\_99_TestUpload\model\UploadService.java

---
+++ Aoide/src/main/java/com/aoide/member/_99_TestUpload/model/UploadService.java
@@ -0,0 +1,29 @@
+package com.aoide.member._99_TestUpload.model;
+
+import com.aoide.global.dataBaseManipulationObjects.song.JdbcSongDAO;
+import com.aoide.global.dataBaseManipulationObjects.song.SongVO;
+
+public class UploadService {
+	// Fields
+	private TestUploadDAO dao1 = null;
+	private JdbcSongDAO dao2 = null;
+	// Constructors
+
+	public UploadService() {
+		this.dao1 = new TestUploadDAO();
+		this.dao2 = new JdbcSongDAO();
+	}
+	// Methods
+	public Integer saveUpload(SongVO song){
+		return dao1.insert(song);
+	}
+	
+	public void updatePath(SongVO song){
+		dao2.update(song);
+	}
+	
+	public SongVO checkUpload(Integer id){
+		return dao2.findByPrimaryKey(id);
+	}
+	
+}
