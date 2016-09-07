72fe5fa3e593ccec9ae77587a645421ec9bb6e26
ren <ren@ren-PC>
2016-09-07 15:11:20 +0800

    _24_EditBullet

---
+++ Aoide/src/main/java/com/aoide/manager/_24_EditBullet/controller/EditBulletList.java
@@ -0,0 +1,38 @@
+package com.aoide.manager._24_EditBullet.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.aoide.global.dataBaseManipulationObjects.bullet.BulletVO;
+import com.aoide.manager._24_EditBullet.model.BulletService;
+
+@WebServlet("/EditBulletList")
+public class EditBulletList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// System.out.println("In ReplySuggestionsServlet doGet()");
+		// get the suggestion list from service
+		List<BulletVO> unreplyBullet = new BulletService().getUnreplyBulletVO();
+        //包入BulletList 
+		request.getSession().setAttribute("BulletList", unreplyBullet);
+		
+		// go suggestionList.jsp
+		String contextPath = request.getContextPath();
+		response.sendRedirect(contextPath
+				+ "/_24_EditBullet.view/BulletList.jsp");
+	}// end doGet()
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// System.out.println("In ReplyBulletServlet doPost()");
+	}
+
+}
---
+++ Aoide/src/main/java/com/aoide/manager/_24_EditBullet/model/BulletService.java
@@ -0,0 +1,38 @@
+package com.aoide.manager._24_EditBullet.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aoide.global.dataBaseManipulationObjects.bullet.BulletDAO;
+import com.aoide.global.dataBaseManipulationObjects.bullet.BulletVO;
+import com.aoide.global.dataBaseManipulationObjects.bullet.JdbcBulletDAO;
+
+
+public class BulletService {
+
+	// Fields
+	BulletDAO bulletDAO = new JdbcBulletDAO();
+
+	// Constructors
+	public BulletService() {
+	}
+	
+	// Methods
+	public List<BulletVO> getUnreplyBulletVO() {
+		List<BulletVO> unreplyBullet = new ArrayList();
+		for (BulletVO aBullet : bulletDAO.getAll()) {
+			unreplyBullet.add(aBullet);
+
+		}// end for
+		return unreplyBullet;
+	}// end getUnreplySuggestions()
+
+	public void updateBullet(BulletVO bulletVO) {
+		bulletDAO.update(bulletVO);
+	}
+
+	public BulletVO getBulletById(Integer bulletId) {
+		return bulletDAO.findByPrimaryKey(bulletId);
+	}
+
+}// end class
