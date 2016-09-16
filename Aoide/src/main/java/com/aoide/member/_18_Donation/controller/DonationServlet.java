09a489fb27c80f1af94f6221e7c2206b23eccf90
Billy <Billy@yahoo.com.tw>
2016-09-16 13:42:08 +0800

    更新Validator.java->新增判斷是否為全數字

--- Aoide/src/main/java/com/aoide/member/_18_Donation/controller/DonationServlet.java
+++ Aoide/src/main/java/com/aoide/member/_18_Donation/controller/DonationServlet.java
@@ -1,12 +1,16 @@
 package com.aoide.member._18_Donation.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.aoide.global._00_TestUtil.Validator;
 import com.aoide.global.dataBaseManipulationObjects.cashRecord.CashRecordVO;
 import com.aoide.global.dataBaseManipulationObjects.member.MemberVO;
 import com.aoide.member._18_Donation.model.DonationService;
@@ -24,7 +28,18 @@ public class DonationServlet extends HttpServlet {
 		//從SESSION來的值，為被贊助者ID
 		Integer recipient_id = 2;
 		//從JSP來的值，贊助金額
-		Integer cash_volume =  Integer.valueOf(request.getParameter("cash_volume"));
+		String cash_volume =  request.getParameter("cash_volume");
+		//驗證贊助金額是否正確
+		if (!Validator.isNumbers(cash_volume)){
+			String cash_volumeError = "請輸入一個正確的數字";
+			request.getSession().setAttribute("cash_volumeError", cash_volumeError);
+			
+			
+			String contextPath = request.getContextPath();
+			response.sendRedirect(contextPath + "/views/member/_18_Donation.view/StartDonationPageTemplate.jsp");
+			
+		} else{
+		Integer int_cash_volume = Integer.valueOf(cash_volume);
 		//TYPE 贊助時為1
 		Integer type = 1;
 		
@@ -38,7 +53,7 @@ public class DonationServlet extends HttpServlet {
 		//將贊助者ID、被贊助者ID、贊助金額、TYPE加入新的VO物件
 		new_CashRecordVO.setSponsorId(sponsor_id);
 		new_CashRecordVO.setRecipientId(recipient_id);
-		new_CashRecordVO.setCashVolume(cash_volume);
+		new_CashRecordVO.setCashVolume(int_cash_volume);
 		new_CashRecordVO.setType(type);
 		
 		Integer insert_id = new DonationService().insertNewCashRecord(new_CashRecordVO);
@@ -51,7 +66,7 @@ public class DonationServlet extends HttpServlet {
 		response.sendRedirect(contextPath + "/views/member/_18_Donation.view/ShowDonationRecordTemplate.jsp");
 		
 		System.out.println(cashRecordVO.getCashRecordId());
-		
+		}
 	}
 
 	
